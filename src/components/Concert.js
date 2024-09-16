9665c832a32f306fcea5be6c3d69bc2d14453fad
Met0l <pepenarmaria@gmail.com>
2024-09-16 13:17:50 +0300

    control commit

--- src/components/Concert.js
+++ src/components/Concert.js
@@ -23,16 +23,16 @@ function Concert(props){
     <>
         <div className='concert-container'>
             <div className='concert-details'>
-                <h2>{props.date}</h2>
-                <h3>{props.venue}</h3>
+              <h2>{props.date}</h2>
+              <h3>{props.venue}</h3>
             </div>
             <div className='concert-location'>  
-                <img src={props.flag} alt={props.country}/>
-                <h3>{props.location}</h3>
+              <img src={props.flag} alt={props.country}/>
+              <h3>{props.location}</h3>
             </div>
             <div className='concert-buttons'>
-                <button onClick={buyTicket} className='buy-button'>Bilete</button>
-                <button onClick={meetGreetClick} className='meetgreet-button'>Meet & Greet</button>
+              <button onClick={buyTicket} className='buy-button'>Bilete</button>
+              <button onClick={meetGreetClick} className='meetgreet-button'>Meet & Greet</button>
             </div>
         </div>
     </>  
